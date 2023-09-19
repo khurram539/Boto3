3718d6f8f85dd2fb599df7d7b83e4f78e64098d8
Your Name <khurram539@hotmail.com>
2023-09-19 19:40:40 +0000

    List all active AWS S3 Buckets

---
+++ S3/main.py
@@ -0,0 +1,23 @@
+import boto3
+
+
+
+#client = boto3.client('s3')
+
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+
+
+
+
+
+
+
+#response = client.list_buckets()
+
+
+
+
+
