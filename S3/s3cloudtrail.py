f3a07af11e2bffc6fde783dee8de296b36900726
Your Name <khurram539@hotmail.com>
2023-11-28 19:47:03 +0000

    update s3cloudtrail.py

--- S3/s3cloudtrail.py
+++ S3/s3cloudtrail.py
@@ -21,3 +21,9 @@ for event in response['Events']:
     bucket_name = event['Resources'][0]['ResourceName']
     user_identity = event['Username']
     print(f"The S3 bucket {bucket_name} was created by {user_identity}")
+
+
+
+In this code, we use the `lookup_events()` method of the CloudTrail client to search for events with the event name `'CreateBucket'`. The response will contain a list of events, and we iterate over each event to retrieve the bucket name and user identity from the event data.
+
+Please note that you need to have the necessary permissions to access CloudTrail and S3, and CloudTrail must be enabled and configured to capture the necessary events for this information to be available.
