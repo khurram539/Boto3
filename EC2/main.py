e1e4ebfd3b000ede5d491d6002868dba1b04a8c9
Your Name <khurram539@hotmail.com>
2023-12-01 18:13:25 +0000

    updated EC2 script

--- EC2/main.py
+++ EC2/main.py
@@ -1,26 +1,19 @@
-# Look up all EC2 instances, instance names, and instance types          
 import boto3
 
-# Create an EC2 client
-ec2 = boto3.client('ec2')
+# Create an EC2 client using the default credential provider chain
+ec2_client = boto3.client('ec2')
 
-# Call describe_instances(), which returns a dictionary
-response = ec2.describe_instances()
+# Retrieve all EC2 instances
+response = ec2_client.describe_instances()
 
-# Loop through the Reservations list in the dictionary
-for reservation in response["Reservations"]:
-    # Loop through the Instances list in the dictionary
-    for instance in reservation["Instances"]:
-        # Initialize variables for instance name and type
-        instance_name = ""
-        instance_type = ""
-        # Loop through the Tags list in the dictionary
-        for tag in instance["Tags"]:
-            # Check if the tag key is "Name"
-            if tag["Key"] == "Name":
-                # Set the instance name
-                instance_name = tag["Value"]
-        # Set the instance type
-        instance_type = instance["InstanceType"]
-        # Print the instance ID, name, and type
-        print(f"Instance ID: {instance['InstanceId']}, Name: {instance_name}, Type: {instance_type}")
+# Iterate over the reservations and instances
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_id = instance['InstanceId']
+        instance_type = instance['InstanceType']
+        state = instance['State']['Name']
+        
+        print(f"Instance ID: {instance_id}")
+        print(f"Instance Type: {instance_type}")
+        print(f"State: {state}")
+        print("------")
