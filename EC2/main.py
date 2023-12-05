8ac0a4b04850178298f221cf39724e20c0b1f090
Your Name <khurram539@hotmail.com>
2023-12-05 20:46:45 +0000

    updated EC2 Script

--- EC2/main.py
+++ EC2/main.py
@@ -1,19 +1,28 @@
 import boto3
 
-# Create an EC2 client using the default credential provider chain
-ec2_client = boto3.client('ec2')
+# Create an EC2 client
+ec2 = boto3.client('ec2')
 
-# Retrieve all EC2 instances
-response = ec2_client.describe_instances()
+# Retrieve information about instances
+response = ec2.describe_instances()
 
-# Iterate over the reservations and instances
+# Extract the required information from the response
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         instance_id = instance['InstanceId']
         instance_type = instance['InstanceType']
-        state = instance['State']['Name']
+        instance_state = instance['State']['Name']
+        vpc_id = instance['VpcId']
+        subnet_id = instance['SubnetId']
+        public_ip = instance.get('PublicIpAddress', 'N/A')
+        private_ip = instance.get('PrivateIpAddress', 'N/A')
         
+        # Print the instance details
         print(f"Instance ID: {instance_id}")
         print(f"Instance Type: {instance_type}")
-        print(f"State: {state}")
-        print("------")
+        print(f"Instance State: {instance_state}")
+        print(f"VPC ID: {vpc_id}")
+        print(f"Subnet ID: {subnet_id}")
+        print(f"Public IP: {public_ip}")
+        print(f"Private IP: {private_ip}")
+        print()
