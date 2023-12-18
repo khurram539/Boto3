b20ec3641500bd47b2e340a3f83ee1e330d564fc
Your Name <khurram539@hotmail.com>
2023-12-18 22:47:27 +0000

    update dele

--- EC2/delec2.py
+++ EC2/delec2.py
@@ -1,5 +1,12 @@
+# import boto3
+# ec2 = boto3.resource('ec2')
+# instance_id = 'i-0ce11afb16ba5ebd8' # replace with your instance ID
+# response = ec2.terminate_instances(InstanceIds=[instance_id])
+# print(response)
+
+
 import boto3
 ec2 = boto3.resource('ec2')
-instance_id = 'i-1234567890abcdef0' # replace with your instance ID
-response = ec2.terminate_instances(InstanceIds=[instance_id])
+instance_id = 'i-0ce11afb16ba5ebd8'
+response = ec2.instances.filter(InstanceIds=[instance_id]).terminate()
 print(response)
