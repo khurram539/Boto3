4388dd9bf0a9aad8ef40d5ae679473eb48a32cce
Your Name <khurram539@hotmail.com>
2024-09-17 17:46:43 -0400

    add more buckets to IG

--- S3/instant_glacier.py
+++ S3/instant_glacier.py
@@ -2,26 +2,39 @@ import boto3
 
 # Initialize the S3 client
 s3 = boto3.client('s3')
-bucket_name = 'aws-163544304364-my-drive'
 
-# List all objects in the bucket
-response = s3.list_objects_v2(Bucket=bucket_name)
+# List of S3 bucket names
+bucket_names = [
+    'aws-163544304364-my-drive',
+    'aws-163544304364-repo',
+    'aws-163544304364-keys',
+    'aws-163544304364-billing',
+    'aws-163544304364-devbox'
+    # Add more bucket names as needed
+]
 
-if 'Contents' in response:
-    for obj in response['Contents']:
-        key = obj['Key']
-        
-        # Copy the object to the same location with the storage class set to Glacier
-        s3.copy_object(
-            Bucket=bucket_name,
-            CopySource={'Bucket': bucket_name, 'Key': key},
-            Key=key,
-            StorageClass='GLACIER_IR' # Glacier Infrequent Access storage class
-        )
-        
-        # Optionally, delete the original object if needed
-        # s3.delete_object(Bucket=bucket_name, Key=key)
+# Iterate over each bucket
+for bucket_name in bucket_names:
+    print(f"Processing bucket: {bucket_name}")
+    
+    # List all objects in the bucket
+    response = s3.list_objects_v2(Bucket=bucket_name)
 
-    print("All objects have been transitioned to Glacier storage class.")
-else:
-    print("No objects found in the bucket.")
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            key = obj['Key']
+            
+            # Copy the object to the same location with the storage class set to Glacier
+            s3.copy_object(
+                Bucket=bucket_name,
+                CopySource={'Bucket': bucket_name, 'Key': key},
+                Key=key,
+                StorageClass='GLACIER_IR' # Glacier Infrequent Access storage class
+            )
+            
+            # Optionally, delete the original object if needed
+            # s3.delete_object(Bucket=bucket_name, Key=key)
+
+        print(f"All objects in bucket {bucket_name} have been transitioned to Glacier storage class.")
+    else:
+        print(f"No objects found in bucket {bucket_name}.")
